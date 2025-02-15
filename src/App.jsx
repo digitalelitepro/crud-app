3d3aacb1100150637d6a26070c6e0c010e206df0
Digital Elite Pro <digitalelitepro@gmail.com>
2025-02-15 10:38:43 +0000

    Installation and firt setup before dive into the developpement

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+ 
+ 
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <div className='App'>
+        Welcome to the React Redux Crud Application
+      </div>
+      
+    </>
+  )
+}
+
+export default App
